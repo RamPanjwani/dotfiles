baf7ed4d5dc67e9faa56fb25eb9dd391711a2711
ram <ram15.panjwani@gmail.com>
2024-12-15 11:32:26 +0530

    Added Powershell script

---
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,2 @@
+oh-my-posh init pwsh --config 'C:\Users\Panjwani\AppData\Local\Programs\oh-my-posh\themes\catppuccin_mocha.omp.json' | Invoke-Expression
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
